e3969f027ca4e9f28fb7d18d4480b9257b6283b1
Andre <andre.elias.vieira@gmail.com>
2020-07-25 16:41:27 -0300

    Adicionadas algumas classes, importados Carroussel e Google Maps para Flutter

--- lib/mapping.dart
+++ lib/mapping.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+class Mapping extends StatefulWidget {
+  @override
+  _MappingState createState() => _MappingState();
+}
+
+class _MappingState extends State<Mapping> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
--- lib/newsfeed.dart
+++ lib/newsfeed.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 //Caroussel at homepage
+
+class NewsFeed extends StatefulWidget {
+  @override
+  _NewsFeedState createState() => _NewsFeedState();
+}
+
+class _NewsFeedState extends State<NewsFeed> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
